215ac9966f70ca3ed717d22679856fa8e0f9bb5a
nazwastpn <nazwasetiapin@gmail.com>
2025-12-02 13:02:05 +0700

    Add model+migration Task

---
+++ database/migrations/2025_12_02_054607_create_tasks_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('name'); // nama tugas
+            $table->boolean('status')->default(false); //status tugas
+            $table->integer('priority')->default(3); //prioritas: low/medium/high
+            $table->date('due_date')->nullable(); // tanggal tugas
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
